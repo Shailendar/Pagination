e2a64a92e1e4419ceb4bc2f57d462dc26f14a845
Shailendar Kanchanapalli <shailendar@COSYN.IN>
2018-08-04 11:34:13 +0530

    Add project files.

---
+++ Pagination.DA/UserDBContext.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration.Conventions;
+
+namespace Pagination.DA
+{
+    public class UserDBContext : DbContext
+    {
+        public UserDBContext() :
+            base("name=DefaultConnection") { }
+        public virtual DbSet<User> User { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
---
+++ Pagination/Controllers/HomeController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Pagination.Models;
+using Pagination.DA;
+using System.Data.Entity;
+using PagedList;
+using PagedList.Mvc;
+
+namespace Pagination.Controllers
+{
+    public class HomeController : Controller
+    {
+        UserDBContext db = new UserDBContext();
+        // GET: Home
+        public ActionResult Index(int? page)
+        {
+            var userList=db.User.Select(m => new UserViewModel 
+            { 
+                Id = m.Id, 
+                FirstName = m.FirstName, 
+                LastName = m.LastName, 
+                Email = m.Email, 
+                Address = m.Address, 
+                Phone = m.Phone
+            }).ToList();
+            int pageSize = 1;
+            int pageNumber = (page ?? 1);
+            return View(userList.ToPagedList(pageNumber,pageSize));
+        }
+    }
+}
